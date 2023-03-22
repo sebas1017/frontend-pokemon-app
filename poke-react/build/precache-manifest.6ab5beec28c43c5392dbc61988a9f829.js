5e06f56d2213c6e9443cd6cc5df898608f3fc2f3
sebastianhenao <sebastianhenao@habi.co>
2023-03-22 18:37:58 -0500

    build files deploy

---
+++ poke-react/build/precache-manifest.6ab5beec28c43c5392dbc61988a9f829.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc872a95883beefff5f312b4c4a4080f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5721439c0784e5da7fb0",
+    "url": "/static/css/main.4c4619ce.chunk.css"
+  },
+  {
+    "revision": "5d85b76d38376ba6bbeb",
+    "url": "/static/js/2.dc26f645.chunk.js"
+  },
+  {
+    "revision": "5721439c0784e5da7fb0",
+    "url": "/static/js/main.aa04df4a.chunk.js"
+  },
+  {
+    "revision": "040cb37eb74ee05bf1cf",
+    "url": "/static/js/runtime-main.c8786d92.js"
+  }
+]);
